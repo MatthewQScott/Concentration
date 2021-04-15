42193500d664fdae267beb71de679ca725d46fda
MatthewCPP <redcanary123@gmail.com>
2021-04-14 20:42:11 -0700

    grid of images and flipping animation

--- app/src/main/java/com/theguild/cs2450/concentration/GameActivity.java
+++ app/src/main/java/com/theguild/cs2450/concentration/GameActivity.java
@@ -7,13 +7,39 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.GridView;
 
-public class GameActivity extends AppCompatActivity {
+
+
+public class GameActivity extends FragmentActivity {
 
     private Button mBackButton;
+    private boolean showingBack = true;
+
+    public static class CardFrontFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_card_front, container, false);
+
+        }
+    }
+
+    public static class CardBackFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_card_back, container, false);
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +47,54 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
 
 
-        mBackButton = (Button) findViewById(R.id.back_button);
-        mBackButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
+         GridView gridView = (GridView) findViewById(R.id.gridView);
+        gridView.setAdapter(new ImageAdapter(this));
+
+
+            if (savedInstanceState == null) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .add(R.id.container, new CardFrontFragment())
+                        .commit();
+
             }
-        });
+
+             mBackButton = (Button) findViewById(R.id.back_button);
+             mBackButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                       // finish();
+                        showingBack = !showingBack;
+                        flipCard();
+
+                    }
+                });
+
+
+}
+
+
+
+    private void flipCard() {
+        if (showingBack) {
+            getSupportFragmentManager().popBackStack();
+            return;
+        }
+
+        getSupportFragmentManager()
+                .beginTransaction()
+
+                .setCustomAnimations(
+                        R.animator.card_flip_right_in,
+                        R.animator.card_flip_right_out,
+                        R.animator.card_flip_left_in,
+                        R.animator.card_flip_left_out)
+
+                .replace(R.id.container, new CardBackFragment())
+
+                .addToBackStack(null)
+
+                .commit();
+
     }
 }
