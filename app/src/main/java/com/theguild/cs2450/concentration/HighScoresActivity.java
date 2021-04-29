b745e87051f4d0601fcb2c31b40e42b32fd94620
ginamtz98 <35149769+ginamtz98@users.noreply.github.com>
2021-04-28 22:55:35 -0700

    Added high score feature

--- app/src/main/java/com/theguild/cs2450/concentration/HighScoresActivity.java
+++ app/src/main/java/com/theguild/cs2450/concentration/HighScoresActivity.java
@@ -1,28 +1,73 @@
 package com.theguild.cs2450.concentration;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.widget.TextView;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class HighScoresActivity extends AppCompatActivity {
 
-    private Button mBackButton;
+    int lastScore;
+    int highScore1;
+    int highScore2;
+    int highScore3;
+    TextView textViewScore;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onBackPressed()
+    {
+        Intent i = new Intent(getApplicationContext(), Game.class);
+        startActivity(i);
+        finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_scores);
+        textViewScore = (TextView) findViewById(R.id.textViewScore);
+        SharedPreferences prefs = getSharedPreferences("PREFS", 0);
+        lastScore = prefs.getInt("Score", 0);
+        highScore1 = prefs.getInt("HighScore1", 0);
+        highScore2 = prefs.getInt("HighScore2", 0);
+        highScore3 = prefs.getInt("HighScore3", 0);
 
-        mBackButton = (Button) findViewById(R.id.back_button);
-        mBackButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+        // updates high scores
+        if(lastScore > highScore3)
+        {
+            highScore3 = lastScore;
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putInt("HighScore3", highScore3);
+            editor.apply();
+        }
+
+        if(lastScore > highScore2)
+        {
+            int temp = highScore2;
+            highScore2 = lastScore;
+            highScore3 = temp;
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putInt("HighScore3", highScore3);
+            editor.putInt("HighScore2", highScore2);
+            editor.apply();
+        }
+
+        if(lastScore > highScore1)
+        {
+            int temp = highScore1;
+            highScore1 = lastScore;
+            highScore2 = temp;
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putInt("HighScore2", highScore2);
+            editor.putInt("HighScore1", highScore1);
+            editor.apply();
+        }
+
+        textViewScore.setText("Score: " + lastScore + "\nHighScore1: " + highScore1 + "\nHighScore2: " + highScore2 + "\nHighScore3: " + highScore3);
 
     }
+
+
 }
