02038fca0f5cbb869533b3ed825cfaac6cca5113
MatthewCPP <redcanary123@gmail.com>
2021-05-07 11:38:56 -0700

    Testing merging the classes into one

--- app/src/main/java/com/theguild/cs2450/concentration/ImageAdapter.java
+++ app/src/main/java/com/theguild/cs2450/concentration/ImageAdapter.java
@@ -1,22 +1,28 @@
 package com.theguild.cs2450.concentration;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
 
 public class ImageAdapter extends BaseAdapter {
+
     private GameActivity mGameActivity;
-    private Game mGame;
+    private ArrayList<Integer> mCardFronts;
 
-
-    // Contructor
-    public ImageAdapter(Context c) {
+    public ImageAdapter(Context c, ArrayList<Integer> cardList) {
         mGameActivity = (GameActivity) c;
+        mCardFronts = cardList;
     }
 
     public int getCount() {
-        return mGame.getNumberOfCards();
+        return mCardFronts.size();
     }
 
     public Object getItem(int position) {
@@ -29,12 +35,22 @@ public class ImageAdapter extends BaseAdapter {
 
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        return (View) mGame.getCardAt(position).getView();
+        ImageView imageView;
+        if (convertView == null) {
+            imageView = new ImageView(mGameActivity);
+
+            imageView.setImageResource(mCardFronts.get(position));
+        } else {
+            imageView = (ImageView) convertView;
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT);
+            convertView.setLayoutParams(new GridView.LayoutParams(params));
+        }
+
+        return imageView;
     }
 
-    public void setGame(Game g) {
-        mGame = g;
-    }
+
+
 }
 
 
